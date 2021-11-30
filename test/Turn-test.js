85b1abcad73556e8432877d5cff59aa5d8c556bd
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-11-30 14:59:32 -0800

    Add the word 'class' to beforeEach description

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -8,7 +8,7 @@ const data = require('../src/data.js').prototypeData;
 describe('Turn', function() {
   let card = null;
   let turn = null;
-  beforeEach('Create an instance of the Card and Turn', function() {
+  beforeEach('Create an instance of the Card and Turn class', function() {
     card = new Card(data[1].id, data[1].question, data[1].answers, data[1].correctAnswer);
     turn = new Turn('array', card);
   });
