2484f5ab1d90147af3e6276cd18f3582351991fe
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-11-30 15:54:57 -0800

    Add import statements for Card.js and data.js

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -2,3 +2,5 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Deck = require('../src/Deck.js');
+const Card = require('../src/Card.js');
+const data = require('../src/data.js').prototypeData;
