a833ab9f744012b01ebcdcbc419eb752d9b1bfaa
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-11-29 19:57:53 -0800

    Add white space before expect statements

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -8,18 +8,22 @@ describe('Turn', function() {
 
   it('should be a function', function() {
     const turn = new Turn();
+
     expect(Turn).to.be.a('function');
   });
 
   it('should be an instance of Turn', function() {
     const turn = new Turn();
+
     expect(turn).to.be.an.instanceof(Turn);
   });
 
   it('should store a user\'s guess', function() {
     const card = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
     const turn = new Turn('array', card);
-
+    
     expect(turn.guess).to.equal('array');
   });
+
+
 });
