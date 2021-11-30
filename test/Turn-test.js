60f57fe845fda66c56e291954c23ccb6c1cead0b
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-11-29 20:26:05 -0800

    Add test to see if Turn can evaluate a guess matches the correct answer

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -49,4 +49,13 @@ describe('Turn', function() {
 
     expect(currentCard).to.equal(card);
   });
+
+  it('should be able to evaluate if a user\'s guess matches the correct answer on the card', function() {
+    const card = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const turn = new Turn('array', card);
+
+    const doesMatch = turn.evaluateGuess();
+
+    expect(doesMatch).to.equal(true);
+  });
 });
