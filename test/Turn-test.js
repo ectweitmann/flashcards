272fdbbc3fccf8dc5be1503749eade29447c73a4
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-11-29 20:02:49 -0800

    Add test to check if Turn stores current card

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -21,9 +21,14 @@ describe('Turn', function() {
   it('should store a user\'s guess', function() {
     const card = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
     const turn = new Turn('array', card);
-    
+
     expect(turn.guess).to.equal('array');
   });
 
+  it('should store the current card in play', function() {
+    const card = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const turn = new Turn('array', card);
 
+    expect(turn.currentCard).to.equal(card);
+  })
 });
