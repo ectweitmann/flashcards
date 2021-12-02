f7005ae1ec7720538374607050540957e6432464
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-12-02 14:11:45 -0800

    Refactor hooks and add test for initilization of new Round object

--- test/Game-test.js
+++ test/Game-test.js
@@ -4,14 +4,18 @@ const expect = chai.expect;
 const Game = require('../src/Game.js');
 const Card = require('../src/Card.js');
 const Deck = require('../src/Deck.js');
+const Round = require('../src/Round.js');
 const data = require('../src/test-data.js').testData;
 
 describe('Game', function () {
   let game;
-  before('Declare and initiliaze class instances', function() {
+  before('Declare and initiliaze Game instance', function() {
     game = new Game();
-    game.start();
   });
+
+  afterEach('Invoke start() method', function() {
+    game.start();
+  })
 
   it('should keep track of the current round', function() {
     expect(game.currentRound).to.equal(null);
@@ -27,5 +31,10 @@ describe('Game', function () {
     expect(game).to.have.property('deck');
     expect(game.deck).to.be.an.instanceof(Deck);
     expect(game.deck.cards).to.deep.equal(game.cards);
+  });
+
+  it('should create a new Round using the deck', function() {
+    expect(game.currentRound).to.be.an.instanceof(Round);
+    expect(game.currentRound.deck).to.deep.equal(game.deck.cards);
   });
 });
