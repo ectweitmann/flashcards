41991d2c153cb5ffc3bf7ca4d0ef8392c8d59793
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-12-01 18:37:43 -0800

    Add takeTurn method

--- src/Round.js
+++ src/Round.js
@@ -1,13 +1,25 @@
+const Turn = require('../src/Turn.js');
+
 class Round {
   constructor(deck) {
-    this.deck = deck;
-    this.currentCard = deck[0];
+    this.deck = deck.cards;
+    this.currentCard = this.deck[0];
     this.turns = 0;
     this.incorrectGuesses = [];
   }
 
   returnCurrentCard() {
     return this.currentCard;
+  }
+
+  takeTurn(guess) {
+    let currentTurn = new Turn(guess, this.currentCard);
+    this.turns++;
+    if (!currentTurn.evaluateGuess()) {
+      this.incorrectGuesses.push(currentTurn.currentCard.id);
+    }
+    this.currentCard = this.deck[this.currentCard.id];
+    return currentTurn.giveFeedback();
   }
 }
 
