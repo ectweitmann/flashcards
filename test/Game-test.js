cd5c5f11a62fb2e92db34a945b9545da6d108058
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-12-02 13:50:32 -0800

    Add test for cards property of Game class

--- test/Game-test.js
+++ test/Game-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Game = require('../src/Game.js');
+const Card = require('../src/Card.js');
 const data = require('../src/test-data.js').testData;
 
 describe('Game', function () {
@@ -13,5 +14,11 @@ describe('Game', function () {
 
   it('should keep track of the current round', function() {
     expect(game.currentRound).to.equal(null);
+  });
+
+  it('should create cards', function() {
+    expect(game).to.have.property('cards');
+    expect(game.cards).to.have.lengthOf(30);
+    expect(game.cards[0]).to.be.an.instanceof(Card);
   });
 });
