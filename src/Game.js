74aa84040c325cc53ad75e95daf2af636e4607fe
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-12-02 14:28:16 -0800

    Add start method

--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,6 @@
+const Card = require('./Card.js');
+const Deck = require('./Deck.js');
+const Round = require('./Round.js');
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
@@ -5,6 +8,14 @@ const util = require('./util');
 class Game {
   constructor() {
     this.currentRound = null;
+  }
+
+  start() {
+    this.cards = prototypeQuestions.map(card => new Card(card.id, card.question, card.answers, card.correctAnswer));
+    this.deck = new Deck(this.cards);
+    this.currentRound = new Round(this.deck);
+    this.printMessage(this.deck, this.currentRound);
+    this.printQuestion(this.currentRound);
   }
 
   printMessage(deck, round) {
